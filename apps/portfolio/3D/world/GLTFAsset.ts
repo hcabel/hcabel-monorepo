9f5b84401d54ecd4cfeb43a3fc950f6341266d4c
hcabel <hcabel@student.42.fr>
2022-09-14 12:30:44 +1000

    Refont of the Assets loading method
    Was too complicated so I create an assets class,
    who you give the path of the model and call the loading fucntion
    the class will emit an event when the loading is finish.

---
+++ apps/portfolio/3D/world/GLTFAsset.ts
@@ -0,0 +1,65 @@
+
+import EventEmitter from "events";
+import * as THREE from "three";
+
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
+
+class GLTFAsset extends EventEmitter {
+
+	private static _GLTFLoader: GLTFLoader;
+	private static _DRACOLoader: DRACOLoader;
+	private _FilePath: string;
+	private _Content: any | undefined;
+
+	get Content(): any | undefined { return this._Content; }
+	get Mesh(): any | undefined { return this._Content?.scene; }
+
+	constructor(filePath: string)
+	{
+		super(); // call EventEmitter constructor
+
+		this._FilePath = filePath;
+
+		// Setup loader
+		if (GLTFAsset._GLTFLoader === undefined || GLTFAsset._DRACOLoader === undefined) {
+			console.log("Setup GLTF loader");
+			GLTFAsset._GLTFLoader = new GLTFLoader();
+			GLTFAsset._DRACOLoader = new DRACOLoader();
+			GLTFAsset._DRACOLoader.setDecoderPath("/draco/");
+			GLTFAsset._GLTFLoader.setDRACOLoader(GLTFAsset._DRACOLoader);
+		}
+	}
+
+	public StartLoading()
+	{
+		GLTFAsset._GLTFLoader.load(this._FilePath, (content) => {
+			this._Content = content;
+			this.emit("loaded");
+		});
+	}
+
+	public UpdateShadow(receiving: boolean, casting: boolean)
+	{
+		if (this._Content === undefined) {
+			return;
+		}
+
+		this._Content.scene.traverse((child) => {
+			if (child.isMesh) {
+				child.castShadow = casting;
+				child.receiveShadow = receiving;
+			}
+			else if (child instanceof THREE.Group) {
+				child.children.forEach((childGroup) => {
+					if (childGroup.isMesh) {
+						childGroup.castShadow = casting;
+						childGroup.receiveShadow = receiving;
+					}
+				});
+			}
+		});
+	}
+}
+
+export default GLTFAsset;
--- apps/portfolio/3D/world/World.ts
+++ apps/portfolio/3D/world/World.ts
@@ -1,15 +1,13 @@
 import * as THREE from 'three';
 
-import Resources from '@3D/utils/Resources';
-
 import Environements from '@3D/world/Environements';
-import { DesktopSceneAsset } from '@3D/utils/Assets';
+import GLTFAsset from './GLTFAsset';
 
 class World {
 
 	// Own properties
 	private _Scene: THREE.Scene;
-	private _Resources: Resources;
+	private _Assets: GLTFAsset[] = [];
 	private _Environements: Environements;
 
 	// Own properties getters
@@ -18,44 +16,52 @@ class World {
 	constructor()
 	{
 		this._Scene = new THREE.Scene();
-		this._Resources = new Resources([ DesktopSceneAsset ]);
+		this._Assets = [
+			new GLTFAsset("models/scene.glb"),
+		];
 
-		this._Resources.on('loaded', () => {
-			this._Environements = new Environements();
+		this.LoadAllAssets()
+			.then(() => {
+				this._Environements = new Environements();
 
-			// Create plane as a floor
-			const plane = new THREE.Mesh(
-				new THREE.PlaneGeometry(100, 100),
-				new THREE.MeshStandardMaterial({
-					color: 0x888888,
-					roughness: 0.75,
-					metalness: 0.5,
-				})
-			);
-			plane.scale.set(10, 10, 10);
-			plane.rotation.x = -Math.PI * 0.5;
-			plane.receiveShadow = true;
-			this._Scene.add(plane);
+				// Create plane as a floor
+				const plane = new THREE.Mesh(
+					new THREE.PlaneGeometry(),
+					new THREE.MeshStandardMaterial({
+						color: 0x888888,
+						roughness: 0.75,
+						metalness: 0.5,
+					})
+				);
+				plane.scale.set(5, 5, 1);
+				plane.rotation.x = Math.PI * -0.5;
+				plane.receiveShadow = true;
+				this._Scene.add(plane);
 
-			const actualScene = this._Resources.Items['DesktopScene'].scene;
-			actualScene.children.forEach((child) => {
-				if (child.name === 'FirstName' || child.name === 'LastName') {
-					return;
-				}
-				child.castShadow = true;
-				child.receiveShadow = true;
-
-				if (child instanceof THREE.Group) {
-					child.children.forEach((groupChild) => {
-						groupChild.castShadow = true;
-						groupChild.receiveShadow = true;
-					});
+				for (const asset of this._Assets) {
+					asset.UpdateShadow(true, true);
+					this._Scene.add(asset.Mesh);
 				}
 			});
-			this._Scene.add(actualScene);
+	}
 
-			// const gridHelper = new THREE.GridHelper();
-			// this._Scene.add( gridHelper );
+	private async LoadAllAssets()
+	{
+		return new Promise<void>((resolve) => {
+			const itemToLoad = this._Assets.length;
+			let itemLoaded = 0;
+
+			console.log("Assets loading...");
+			this._Assets.forEach((asset: GLTFAsset) => {
+				asset.StartLoading();
+				asset.on('loaded', () => {
+					itemLoaded++;
+					if (itemLoaded === itemToLoad) {
+						console.log("Assets loaded!");
+						resolve();
+					}
+				});
+			});
 		});
 	}
 }
