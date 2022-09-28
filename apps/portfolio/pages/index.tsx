b793fdd047931d07dcf78a4535bc175ad3af9811
hcabel <hcabel@student.42.fr>
2022-09-28 15:16:06 +1000

    Wrap each proejcts with a link to there respective platform
    I added a little raise animation on each 'a' element
    I added comment to visualy separet each project

--- apps/portfolio/pages/index.tsx
+++ apps/portfolio/pages/index.tsx
@@ -17,34 +17,49 @@ export function Index() {
 		<section className="Page">
 			<canvas id="Canvas3D" className={Style.ThreeJsCanvas3D}></canvas>
 			<main id="HtmlGridContent" className={Style.HtmlPageContent}>
+				{/* UNREAL VSCODE HELPER */}
 				<ProjectFirstImpression
 					className={Style.ProjectUVCH}
 					projectName="Unreal VsCode Helper"
 					projectDescription="is a VSCode extension that provides a set of tools to help you develop Unreal Engine projects inside VsCode."
 				>
 					<div className={Style.ProjectStats}>
-						<YoutubeStats views={92_197} />
-						<VsCodeStats name="HugoCabel.uvch"/>
-						<GithubStats repoUrl="https://github.com/hcabel/UnrealVsCodeHelper" />
+						<a href="https://www.youtube.com/watch?v=_usDZ6osnR4">
+							<YoutubeStats views={92_197} />
+						</a>
+						<a href="https://marketplace.visualstudio.com/items?itemName=HugoCabel.uvch">
+							<VsCodeStats name="HugoCabel.uvch"/>
+						</a>
+						<a href="https://github.com/hcabel/UnrealVsCodeHelper">
+							<GithubStats repoUrl="https://github.com/hcabel/UnrealVsCodeHelper" />
+						</a>
 					</div>
 				</ProjectFirstImpression>
+				{/* HUGO MEET */}
 				<ProjectFirstImpression
 					className={Style.ProjectHugoMeet}
 					projectName="Hugo Meet"
 					projectDescription="is a video meeting platform, that I made to learn how to use WebRTC and video/audio streaming."
 				>
 					<div className={Style.ProjectStats}>
-						<YoutubeStats views={1_154 + 218} />
-						<GithubStats repoUrl="https://github.com/hcabel/HugoMeet" />
+						<a href="https://www.youtube.com/watch?v=2oupECsHxPU">
+							<YoutubeStats views={1_154 + 218} />
+						</a>
+						<a href="https://github.com/hcabel/HugoMeet">
+							<GithubStats repoUrl="https://github.com/hcabel/HugoMeet" />
+						</a>
 					</div>
 				</ProjectFirstImpression>
+				{/* PROCEDURAL TERRAIn */}
 				<ProjectFirstImpression
 					className={Style.ProjectProceduralTerrain}
 					projectName="Procedural Terrain"
 					projectDescription="This is a Unreal Engine actor who's gonna generate infinite terrain in any of your game. (Like Minecraft)"
 				>
 					<div className={Style.ProjectStats}>
-						<YoutubeStats views={7_034} />
+						<a href="https://www.youtube.com/watch?v=MHB8Tn3zbqM">
+							<YoutubeStats views={7_034} />
+						</a>
 					</div>
 				</ProjectFirstImpression>
 			</main>
