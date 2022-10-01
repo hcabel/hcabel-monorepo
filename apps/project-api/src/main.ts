6f2dd4dd8b74ebf63e35789feae0bd411859a97c
hcabel <hcabel@student.42.fr>
2022-10-01 18:54:50 +1000

    Implement Mongoose

--- apps/project-api/src/main.ts
+++ apps/project-api/src/main.ts
@@ -1,4 +1,6 @@
 import * as express from "express";
+import mongoose from "mongoose";
+
 import RouteTree from "./routes/routes";
 import { GenerateRouterFromRoutingTree } from "@hcabel/rest-api-utils";
 
@@ -7,8 +9,16 @@ const app = express();
 const router = GenerateRouterFromRoutingTree(RouteTree);
 app.use('/', router);
 
-const port = process.env.port || 3333;
-const server = app.listen(port, () => {
-	console.log(`Listening at ${process.env.NX_PROJECTAPI_ENDPOINT}`);
+const dbUri = process.env.NX_MONGO_URI;
+mongoose.connect(dbUri || "", {
+	w: "majority",
+	appName: "ProjectApi",
+}, () => {
+	console.log('Mongo ready');
+
+	const port = process.env.port || 3333;
+	const server = app.listen(port, () => {
+		console.log(`Listening at ${process.env.PROJECTAPI_ENDPOINT}`);
+	});
+	server.on("error", console.error);
 });
-server.on("error", console.error);
