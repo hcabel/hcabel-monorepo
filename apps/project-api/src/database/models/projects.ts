56726e8fc8ea24f07ab0365c85b4cc715f5e4d14
hcabel <hcabel@student.42.fr>
2022-10-01 20:08:37 +1000

    Remove stats from the project model.
    I read that it has to be the other way around.

--- apps/project-api/src/database/models/projects.ts
+++ apps/project-api/src/database/models/projects.ts
@@ -2,9 +2,8 @@ import {Schema, Types, model} from 'mongoose';
 
 // Project interface Schemat
 export interface ISProject {
-	userId: Types.ObjectId,
-	value: string,
-	expirationDate: number
+	name: string,
+	description: string,
 }
 
 const ProjectSchema = new Schema({
@@ -15,17 +14,14 @@ const ProjectSchema = new Schema({
 	description: {
 		type: String,
 		required: true
-	},
-	stats: {
-		type: [Types.ObjectId],
-		required: false,
-		default: []
 	}
 });
 
 // Project interface model (because by default mongo add a _id field)
 export interface IMProject extends ISProject {
-	_id: Types.ObjectId
+	_id: Types.ObjectId,
 }
 
-export default model<IMProject>("tokens", ProjectSchema);
+export const Project = model<IMProject>("Project", ProjectSchema);
+
+export default Project;
