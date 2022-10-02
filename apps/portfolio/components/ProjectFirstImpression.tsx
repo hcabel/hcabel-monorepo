bf7a4dee42e15252a381f55e3236b2c605c6e4b1
hcabel <hcabel@student.42.fr>
2022-10-03 08:56:04 +1000

    The portfolio is now fetching the projects info in the DB using the ProjectAPI
    I moved all the stats back into the ProjectFirstImpression component,
    and he will fetch all the project info by himself.

--- apps/portfolio/components/ProjectFirstImpression.tsx
+++ apps/portfolio/components/ProjectFirstImpression.tsx
@@ -1,4 +1,12 @@
+import { useEffect, useState } from 'react';
+
 import Style from 'Styles/components/ProjectFirstImpression.module.scss';
+
+import { IGetProjectInfos } from '@hcabel/types/ProjectApi';
+
+import GithubStats from 'Components/ProjectStats/GithubStats';
+import VsCodeStats from 'Components/ProjectStats/VsCodeStats';
+import YoutubeStats from 'Components/ProjectStats/YoutubeStats';
 
 export interface IProjectFirstImpressionProps {
 	id?: string;
@@ -7,20 +15,32 @@ export interface IProjectFirstImpressionProps {
 	children?: React.ReactElement[] | React.ReactElement;
 
 	projectName: string;
-	projectDescription: string;
 	onMoreClicked?: () => void;
 }
 
 export default function ProjectFirstImpression(props: IProjectFirstImpressionProps)
 {
+	const [_Project, set_Project] = useState<IGetProjectInfos | undefined>(undefined);
+
+	useEffect(() => {
+		fetch(`api/projects/${props.projectName}`)
+			.then((response) => response.json())
+			.then((data: IGetProjectInfos) => set_Project(data));
+	}, [ props.projectName ]);
+
+	// If we dont have the project infos, we dont render anything
+	// @TODO: Implement Client Side Rendering
+	if (!_Project) {
+		return null;
+	}
 	return (
 		<article id={props.id || ""} className={`${Style.ProjectFirstImpression} ${props.className || ""}`} style={props.style || {}}>
 			<div>
 				<figure className={Style.ProjectName}>
-					{props.projectName}
+					{_Project.name}
 				</figure>
 				<figcaption className={Style.ProjectDescription}>
-					{props.projectDescription}
+					{_Project.description}
 				</figcaption>
 				{props.onMoreClicked &&
 					<a className={Style.ProjectMoreButton} onClick={props.onMoreClicked}>
@@ -28,7 +48,26 @@ export default function ProjectFirstImpression(props: IProjectFirstImpressionPro
 					</a>
 				}
 			</div>
-			{props.children}
+			<div className={Style.ProjectStats}>
+				<a href="https://www.youtube.com/watch?v=_usDZ6osnR4">
+					<YoutubeStats
+						views={_Project.stats
+							.filter((stat) => {
+								return stat.platform === "youtube";
+							})
+							.reduce((acc, stat) => {
+								return acc + (stat.name === "views" ? stat.value : 0);
+							}, 0)
+						}
+					/>
+				</a>
+				<a href="https://marketplace.visualstudio.com/items?itemName=HugoCabel.uvch">
+					<VsCodeStats name="HugoCabel.uvch"/>
+				</a>
+				<a href="https://github.com/hcabel/UnrealVsCodeHelper">
+					<GithubStats repoUrl="https://github.com/hcabel/UnrealVsCodeHelper" />
+				</a>
+			</div>
 		</article>
 	);
 }
--- apps/portfolio/pages/index.tsx
+++ apps/portfolio/pages/index.tsx
@@ -5,10 +5,6 @@ import Style from 'Styles/pages/index.module.scss';
 import Canvas3D from '3D/Canvas3D';
 import ProjectFirstImpression from 'Components/ProjectFirstImpression';
 
-import GithubStats from 'Components/ProjectStats/GithubStats';
-import YoutubeStats from 'Components/ProjectStats/YoutubeStats';
-import VsCodeStats from 'Components/ProjectStats/VsCodeStats';
-
 export function Index() {
 	useEffect(() => {
 		new Canvas3D(document.getElementById("Canvas3D") as HTMLCanvasElement);
@@ -22,47 +18,17 @@ export function Index() {
 				<ProjectFirstImpression
 					className={Style.ProjectUVCH}
 					projectName="Unreal VsCode Helper"
-					projectDescription="is a VSCode extension that provides a set of tools to help you develop Unreal Engine projects inside VsCode."
-				>
-					<div className={Style.ProjectStats}>
-						<a href="https://www.youtube.com/watch?v=_usDZ6osnR4">
-							<YoutubeStats views={92_197} />
-						</a>
-						<a href="https://marketplace.visualstudio.com/items?itemName=HugoCabel.uvch">
-							<VsCodeStats name="HugoCabel.uvch"/>
-						</a>
-						<a href="https://github.com/hcabel/UnrealVsCodeHelper">
-							<GithubStats repoUrl="https://github.com/hcabel/UnrealVsCodeHelper" />
-						</a>
-					</div>
-				</ProjectFirstImpression>
+				/>
 				{/* HUGO MEET */}
 				<ProjectFirstImpression
 					className={Style.ProjectHugoMeet}
-					projectName="Hugo Meet"
-					projectDescription="is a video meeting platform, that I made to learn how to use WebRTC and video/audio streaming."
-				>
-					<div className={Style.ProjectStats}>
-						<a href="https://www.youtube.com/watch?v=2oupECsHxPU">
-							<YoutubeStats views={1_154 + 218} />
-						</a>
-						<a href="https://github.com/hcabel/HugoMeet">
-							<GithubStats repoUrl="https://github.com/hcabel/HugoMeet" />
-						</a>
-					</div>
-				</ProjectFirstImpression>
+					projectName="HugoMeet"
+				/>
 				{/* PROCEDURAL TERRAIN */}
 				<ProjectFirstImpression
 					className={Style.ProjectProceduralTerrain}
 					projectName="Procedural Terrain"
-					projectDescription="This is a Unreal Engine actor who's gonna generate infinite terrain in any of your game. (Like Minecraft)"
-				>
-					<div className={Style.ProjectStats}>
-						<a href="https://www.youtube.com/watch?v=MHB8Tn3zbqM">
-							<YoutubeStats views={7_034} />
-						</a>
-					</div>
-				</ProjectFirstImpression>
+				/>
 			</main>
 		</section>
 	);
