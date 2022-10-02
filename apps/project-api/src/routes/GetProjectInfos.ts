037448217cc71ce5aacd0fbe9d989c64a08d74d7
hcabel <hcabel@student.42.fr>
2022-10-02 11:17:13 +1000

    Add all stats when getting project infos

--- apps/project-api/src/routes/GetProjectInfos.ts
+++ apps/project-api/src/routes/GetProjectInfos.ts
@@ -1,6 +1,7 @@
 import { IRequestResponse } from "@hcabel/rest-api-utils";
 import { Request } from "express";
-import { GetProjectByName } from "../database/services/services";
+import * as ProjectService from "../database/services/services";
+import * as StatService from "../database/services/stat";
 
 
 async function GetProjectInfos(req: Request): Promise<IRequestResponse>
@@ -15,17 +16,30 @@ async function GetProjectInfos(req: Request): Promise<IRequestResponse>
 		});
 	}
 
-	const infos = await GetProjectByName(projectName);
-	if (!infos) {
+	// get project
+	const project = await ProjectService.GetProjectByName(projectName);
+	if (!project) {
 		return ({
 			status: 404,
 			json: { message: "Project not found" }
 		});
 	}
 
+	// get projects stats
+	const stats = await StatService.GetAllProjectStats(project._id);
+	if (!stats) {
+		return ({
+			status: 404,
+			json: { message: "Stats not found" }
+		});
+	}
+
 	return ({
 		status: 200,
-		json: infos
+		json: {
+			project,
+			stats: stats
+		}
 	});
 }
 
