704d45724e6b074e6cf4b2149226d4192770d64e
hcabel <hcabel@student.42.fr>
2022-10-17 16:01:31 +1000

    Update pipeline

--- tools/scripts/github/workflow/pipeline.js
+++
@@ -1,9 +0,0 @@
-const execSync = require('child_process').execSync;
-
-const target = process.argv[2];
-const baseSha = process.argv[3];
-const headSha = process.argv[4];
-
-execSync(
-	`npx nx affected --target=${target} --base=${baseSha} --head=${headSha} --parallel`,
-).toString('utf-8');
