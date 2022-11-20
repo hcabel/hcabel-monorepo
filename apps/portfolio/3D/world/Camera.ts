b4ff40f6779317deeda36dbabdde5fcb7724826e
hcabel <hcabel@student.42.fr>
2022-11-20 21:27:49 +1000

    Portfolio: Refont smooth rotation/poision systeme
    I was causing many issues like shaking on small movement.
    It was base on dividing the length bewteen were you are and were you want to go by 4 who's not very optimum.
    Example: 0 -> 1 = 0.25 => 0.4375 => 0.578125 => ...
    Now we interpolate from the position were the camera was when the move has been triggered.

--- apps/portfolio/3D/world/Camera.ts
+++ apps/portfolio/3D/world/Camera.ts
@@ -6,16 +6,20 @@ import Control from '3D/world/controls/Controls';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import Experience from '3D/Experience';
 
-export interface ILerp3d {
-	current: THREE.Vector3,
-	target: THREE.Vector3,
+export interface ICameraPosition {
+	start: THREE.Vector3,
+	end: THREE.Vector3,
 	speed: number,
+	progress: number,
 }
 
-export interface ILerpQuad {
-	current: THREE.Quaternion,
-	target: THREE.Quaternion,
+export interface ICameraRotation {
+	start: THREE.Quaternion,
+	end: THREE.Quaternion,
 	speed: number,
+	progress: number,
+	isFocus: boolean,
+	focusPoint: THREE.Vector3,
 }
 
 class Camera
@@ -27,8 +31,10 @@ class Camera
 	// Own properties
 	private _PerspectiveCamera: THREE.PerspectiveCamera;
 	private _Controls: Control;
-	private _PositionLerp: ILerp3d = { current: new THREE.Vector3(0), target: new THREE.Vector3(0), speed: 0.25 };
-	private _RotationLerp: ILerpQuad = { current: new THREE.Quaternion(0), target: new THREE.Quaternion(0), speed: 0.25 };
+	private _Position: ICameraPosition
+		= { start: new THREE.Vector3(0), end: new THREE.Vector3(0), speed: 0.25, progress: 0 };
+	private _Rotation: ICameraRotation
+		= { start: new THREE.Quaternion(0), end: new THREE.Quaternion(0), speed: 0.25, progress: 0, isFocus: false, focusPoint: new THREE.Vector3(0) };
 	private _OrbitControls: OrbitControls | undefined;
 
 	// Own properties getters
@@ -88,27 +94,43 @@ class Camera
 
 		this._Controls.Update();
 
-		// Update smooth position
-		this._PositionLerp.current.lerp(this._PositionLerp.target, this._PositionLerp.speed);
-		if (this._PositionLerp.current.distanceTo(this._PositionLerp.target) < 0.01) {
-			this._PositionLerp.current.copy(this._PositionLerp.target);
-		}
-		this._PerspectiveCamera.position.copy(this._PositionLerp.current);
-
-		// Update smooth rotation
-		this._RotationLerp.current.slerp(this._RotationLerp.target, this._RotationLerp.speed);
-		this._PerspectiveCamera.rotation.setFromQuaternion(this._RotationLerp.current);
+		this.UpdatePosition();
+		this.UpdateRotation();
 	}
 
 	/* LOCATION **************************************************************/
 
+	private UpdatePosition()
+	{
+		if (this._Position.progress < 1) {
+			// Update progress
+			this._Position.progress += this._Position.speed;
+
+			// lerp between start and end
+			this._PerspectiveCamera.position.lerpVectors(
+				this._Position.start,
+				this._Position.end,
+				this._Position.progress);
+
+			// if (this._Position.progress >= 1) {
+			// 	console.log('Camera position lerp done', this._PerspectiveCamera.position);
+			// }
+		}
+	}
+
 	public MoveTo(x: number, y: number, z: number, teleport = false)
 	{
 		if (teleport) {
+			this._Position.progress = 1;
+			this._Position.start.set(x, y, z);
+			this._Position.end.set(x, y, z);
 			this._PerspectiveCamera.position.set(x, y, z);
-			this._PositionLerp.current.set(x, y, z);
 		}
-		this._PositionLerp.target.set(x, y, z);
+		else {
+			this._Position.progress = 0;
+			this._Position.start.copy(this._PerspectiveCamera.position);
+			this._Position.end.set(x, y, z);
+		}
 	}
 
 	public MoveToVector3(pos: THREE.Vector3, teleport = false)
@@ -127,13 +149,55 @@ class Camera
 
 	/* ROTATION **************************************************************/
 
+	private	UpdateRotation()
+	{
+		if (this._Rotation.isFocus) {
+			this._PerspectiveCamera.lookAt(this._Rotation.focusPoint);
+		}
+		else if (this._Rotation.progress < 1) {
+			// Update progress
+			this._Rotation.progress += this._Rotation.speed;
+
+			// lerp between start and end
+			this._PerspectiveCamera.quaternion.slerpQuaternions(
+				this._Rotation.start,
+				this._Rotation.end,
+				this._Rotation.progress);
+
+			if (this._Rotation.progress >= 1) {
+				console.log('Camera rotation lerp done',
+					this._PerspectiveCamera.quaternion.x * 100,
+					this._PerspectiveCamera.quaternion.y * 100,
+					this._PerspectiveCamera.quaternion.z * 100,
+					this._PerspectiveCamera.quaternion.w * 100);
+			}
+		}
+	}
+
+	public Unfocus()
+	{
+		this._Rotation.isFocus = false;
+	}
+
+	public Focus(focusPoint: THREE.Vector3)
+	{
+		this._Rotation.isFocus = true;
+		this._Rotation.focusPoint = focusPoint;
+	}
+
 	public RotateTo(rot: THREE.Quaternion, teleport = false)
 	{
 		if (teleport) {
-			this._PerspectiveCamera.rotation.setFromQuaternion(rot);
-			this._RotationLerp.current.copy(rot);
+			this._Rotation.progress = 1;
+			this._Rotation.start.copy(rot);
+			this._Rotation.end.copy(rot);
+			this._PerspectiveCamera.quaternion.copy(rot);
 		}
-		this._RotationLerp.target.copy(rot);
+		else {
+			this._Rotation.progress = 0;
+			this._Rotation.start.copy(this._PerspectiveCamera.quaternion);
+			this._Rotation.end.copy(rot);
+		}
 	}
 
 	public LookAt(target: THREE.Vector3, teleport = false)
