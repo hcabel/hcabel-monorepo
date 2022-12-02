2ac002e7c2f291fd0e65599912a660b24abacfb0
hcabel <hcabel@student.42.fr>
2022-12-02 14:19:12 +1000

    ProjectApi: Add IDatabase interface

--- apps/project-api/src/database/database.ts
+++ apps/project-api/src/database/database.ts
@@ -1,4 +1,8 @@
 import mongoose from "mongoose";
+import { IDatabase } from "@hcabel/rest-api-utils";
+
+import * as queries from "./queries";
+import { IQueries } from "@hcabel/types/ProjectApi";
 
 function connect()
 {
@@ -7,11 +11,23 @@ function connect()
 			w: "majority",
 			appName: "ProjectApi",
 		})
+			.then((mongoDatabase) => {
+				// check if connection is successfull
+				if (mongoDatabase.connection.readyState === 1 /* connected */) {
+					return (true);
+				}
+				return (false);
+			})
+			.catch((error) => {
+				console.error(error);
+				return (false);
+			})
 	);
 }
 
-const database = {
+const database: IDatabase<IQueries> = {
 	connect,
+	queries: queries
 };
 
 export default database;
--- libs/rest-api-utils/src/index.ts
+++ libs/rest-api-utils/src/index.ts
@@ -1,2 +1,3 @@
 export * from './lib/routing';
 export * from './lib/request';
+export * from './lib/database.interface';
