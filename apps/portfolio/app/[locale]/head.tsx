1783f1833cdab98cb6e9f6403503345b47599f41
hcabel <hcabel@student.42.fr>
2023-01-06 17:15:05 +1000

    portfolio: Merge root and locale layout
    1- I merge the root and the locale layout into a single one.
    Because we dont want any oage before locale otherwise we dont know wich locale to use.
    2- The layout is now a Server side components

--- apps/portfolio/app/[locale]/head.tsx
+++ apps/portfolio/app/[locale]/head.tsx
@@ -1,5 +1,13 @@
 
-export default function Head() {
+interface IHeadProps {
+	title: string,
+	desc: string
+}
+
+export default function Head(props: IHeadProps) {
+	const title = props.title;
+	const desc = props.desc?.replace(/\s+/g, ' ') || "";
+
 	return (
 		<>
 			<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
@@ -8,24 +16,24 @@ export default function Head() {
 			<link rel="manifest" href="/site.webmanifest" />
 
 			{/* HTML Meta Tags */}
-			<title>Hugo Cabel</title>
-			<meta name="description" content="My portfolio, come see my personal projects and what I'm capable of." />
+			<title>{title}</title>
+			<meta name="description" content={desc} />
 			<meta content="width=device-width, initial-scale=1" name="viewport" />
 			<meta name="theme-color" content="#ffffff" />
 
 			{/* Facebook Meta Tags */}
 			<meta property="og:url" content="https://hugocabel.com" />
 			<meta property="og:type" content="website" />
-			<meta property="og:title" content="Hugo Cabel" />
-			<meta property="og:description" content="My portfolio, come see my personal projects and what I'm capable of." />
+			<meta property="og:title" content={title} />
+			<meta property="og:description" content={desc} />
 			<meta property="og:image" content="https://hugocabel.com/android-chrome-512x512.png" />
 
 			{/* Twitter Meta Tags */}
 			<meta name="twitter:card" content="summary_large_image" />
 			<meta property="twitter:domain" content="hugocabel.com" />
 			<meta property="twitter:url" content="https://hugocabel.com" />
-			<meta name="twitter:title" content="Hugo Cabel" />
-			<meta name="twitter:description" content="My portfolio, come see my personal projects and what I'm capable of." />
+			<meta name="twitter:title" content={title} />
+			<meta name="twitter:description" content={desc} />
 			<meta name="twitter:image" content="https://hugocabel.com/android-chrome-512x512.png" />
 		</>
 	);
--- apps/portfolio/app/[locale]/layout.tsx
+++ apps/portfolio/app/[locale]/layout.tsx
@@ -1,19 +1,42 @@
-'use client';
+import CookiePopup from 'Components/CookiePopup/CookiePopup';
+import '../global.scss';
 
-import { useEffect } from 'react';
+import Head from "./head";
+import LocaleSelector from './LocaleSelector';
 
-interface LocaleLayoutProps {
-	children: React.ReactNode;
-	params: {
-		locale: string;
-	};
+export type Locales = "en" | "fr";
+
+export interface LocaleLayoutParams {
+	locale: Locales;
 }
 
-export default function LocaleLayout(props: LocaleLayoutProps)
-{
-	useEffect(() => {
-		document.documentElement.lang = props.params.locale || "en";
-	}, [props.params.locale]);
+export interface ILocaleLayoutProps {
+	children: React.ReactNode;
+	params: LocaleLayoutParams;
+}
 
-	return (props.children);
+export default function LocaleLayout(props: ILocaleLayoutProps)
+{
+	return (
+		<html lang={props.params.locale}>
+			<Head
+				title={"Hugo Cabel"}
+				desc={"																						\
+					Welcome to my portfolio!																\
+					I am a passionate software engineer that love very diverse fields of computer science.	\
+					On this website, you can browse through my latest projects,								\
+					including 																				\
+						'HugoMeet' (using JavaScript, WebRTC, and React),									\
+						a terrain procedural generation project created with Unreal Engine 4 and C++,		\
+						and much more.																		\
+					Take a look and get in touch if you'd like to work together on your next project!		\
+				"}
+			/>
+			<body className="root" style={{ background: "grey" }}>
+				<LocaleSelector locale={props.params.locale} />
+				{props.children}
+				<CookiePopup />
+			</body>
+		</html>
+	);
 }
