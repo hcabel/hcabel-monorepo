b221f885ab9061d6e0d7a99d9d1456aebb02619a
hcabel <hcabel@student.42.fr>
2023-01-06 17:17:49 +1000

    portfolio: CookiePopup handle own visibility
    I move the cookie show/hide behavior in the CookiePopup client component.
    This allow the locale layout to be a server component and his child too

--- apps/portfolio/components/CookiePopup/CookiePopup.tsx
+++ apps/portfolio/components/CookiePopup/CookiePopup.tsx
@@ -1,17 +1,48 @@
+'use client';
+
 import Image from "next/image";
 
 // Design
 import Style from "./CookiePopup.module.scss";
 import CookiesBackground from "Images/CookiesBackground.png";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import Cookies from "js-cookie";
+import CookieManager from "Utils/CookieManager";
 
-interface ICookiePopupProps {
-	onAccept: () => void,
-	onDeny: () => void
-}
-
-export default function CookiePopup(props: ICookiePopupProps)
+export default function CookiePopup()
 {
+	// Show cookie popup if the cookie has never been setted before
+	const [ _ShowCookiePopup, set_ShowCookiePopup ] = useState<boolean>(false);
+
+	useEffect(() => {
+		if (!Cookies.get("AllowCookies")) {
+			set_ShowCookiePopup(true);
+		}
+	}, []);
+
+	// If the user accept the cookies, set the cookie and hide the popup
+	function	AcceptCookies()
+	{
+		// Create a cookie that remember for a year that the cookie policy was accepted
+		CookieManager.setCookie("AllowCookies", "true", {
+			expires: 365 /* 1 Year */
+		});
+		set_ShowCookiePopup(false);
+	}
+
+	// If the user refuse the cookies, set the cookie (has session cookie) and hide the popup
+	function RefuseCookies()
+	{
+		// Create a session cookie that will be delete at the end of the session
+		CookieManager.setCookie("AllowCookies", "false");
+		set_ShowCookiePopup(false);
+	}
+
+	// Hide popup if the user already accepted or refused the cookies
+	if (_ShowCookiePopup === false) {
+		return (null);
+	}
 	return (
 		<div className={Style.Popup}>
 			<Image
@@ -36,10 +67,10 @@ export default function CookiePopup(props: ICookiePopupProps)
 				</Link>
 			</h5>
 			<div className={Style.Buttons}>
-				<button onClick={props.onDeny}>
+				<button onClick={AcceptCookies}>
 					<h5 className={`h5 ${Style.BtnText}`}>Deny 😞</h5>
 				</button>
-				<button onClick={props.onAccept} className={`${Style.PrimaryBtn}`}>
+				<button onClick={RefuseCookies} className={`${Style.PrimaryBtn}`}>
 					<h5 className={`h5 ${Style.BtnText}`}>Allow 😊</h5>
 				</button>
 			</div>
