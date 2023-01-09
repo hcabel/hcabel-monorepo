128b8e8ba7ec38e884aea005e65c5c6dac7daf30
hcabel <hcabel@student.42.fr>
2023-01-10 07:55:23 +1000

    portfolio: add 3d scene to the uvch project

--- apps/portfolio/app/[locale]/landing/projects/unreal-vscode-helper/page.tsx
+++ apps/portfolio/app/[locale]/landing/projects/unreal-vscode-helper/page.tsx
@@ -6,12 +6,14 @@ import Style from "../(shared)/project.module.scss";
 export default async function ProjectsPage()
 {
 	const project = await GetProjectData("6345e3b3af50b4452641831e", { next: { revalidate: 60 * 60 * 24 /* each day */ }});
+
 	return (
-		<Project
-			project={project}
-			className={Style.Project}
-			moreButtonRedirection="/redirects/unreal-vscode-helper"
-		/>
+			<UvchExperienceCanvas />
+			<Project
+				project={project}
+				className={Style.Project}
+				moreButtonRedirection="/redirects/unreal-vscode-helper"
+			/>
 	);
 }
 
