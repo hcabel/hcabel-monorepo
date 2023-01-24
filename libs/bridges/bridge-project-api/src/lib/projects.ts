77378dffd082f403b8e88ddcf90a73d9ea13773d
hcabel <hcabel@student.42.fr>
2023-01-24 13:43:28 +1000

    bridge-project-api: Fix linting errors

--- libs/bridges/bridge-project-api/src/lib/projects.ts
+++ libs/bridges/bridge-project-api/src/lib/projects.ts
@@ -4,23 +4,25 @@ import {
 	IRouteGetProjectById,
 } from "@hcabel/types/ProjectApi";
 
-export function get_all_projects(queryParamsFilter: `?${string}`,  options?: RequestInit): Promise<IRouteGetAllProjects>
+export function get_all_projects(queryParamsFilter: `?${string}`, options?: RequestInit): Promise<IRouteGetAllProjects>
 {
 	return (
 		fetch(
 			`${process.env.NX_PROJECT_API_ENDPOINT}/projects${queryParamsFilter || ''}`,
 			options || {}
 		)
-		.then((res) => res.json() as Promise<IRouteGetAllProjects>)
-	)
+			.then((res) => res.json() as Promise<IRouteGetAllProjects>)
+	);
 }
-export function get_single_project(projectId: string, options?: RequestInit): Promise<IRouteGetProjectById> {
+
+export function get_single_project(projectId: string, options?: RequestInit): Promise<IRouteGetProjectById>
+{
 	// fetch the project data from the api
 	return (
 		fetch(
 			`${process.env.NX_PROJECT_API_ENDPOINT}/projects/${projectId}`,
 			options || {}
 		)
-		.then((res) => res.json() as Promise<IRouteGetProjectById>)
+			.then((res) => res.json() as Promise<IRouteGetProjectById>)
 	);
 }
