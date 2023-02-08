af083ed1dcb55e5602d108433400a726667be827
hcabel <hcabel@student.42.fr>
2023-02-08 15:56:02 +1000

    Telemetry-api: Allow request only from hugocabel.com

--- apps/backend/telemetry-api/src/app/app.ts
+++ apps/backend/telemetry-api/src/app/app.ts
@@ -11,7 +11,7 @@ export function create_app(database: any) {
 
 	// Allow cross origin requests
 	app.use((req, res, next) => {
-		res.header("Access-Control-Allow-Origin", "*");
+		res.header("Access-Control-Allow-Origin", "https://hugocabel.com");
 		res.header(
 			"Access-Control-Allow-Headers",
 			"Origin, X-Requested-With, Content-Type, Accept"
